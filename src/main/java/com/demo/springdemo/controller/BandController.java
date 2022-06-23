621a5628c7532bb57a4786e8edf3f22bc2a0489d
drgfc <dragostufa_n@yahoo.com>
2022-06-23 12:37:15 +0300

    Band Update

---
+++ src/main/java/com/demo/springdemo/controller/BandController.java
@@ -0,0 +1,23 @@
+package com.demo.springdemo.controller;
+
+import com.demo.springdemo.model.Band;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class BandController {
+    @GetMapping(value = "/band")
+    public String band(Model model) {
+        List<Band> bandList = List.of(
+                new Band("Eagles", "Rock", "USA", 1971, 7),
+                new Band("Metallica", "Heavy metal", "USA", 1981, 10),
+                new Band("ABBA", "Pop", "Sweden", 1972, 9),
+                new Band("The Prodigy", "EDM", "UK", 1990, 7)
+        );
+        model.addAttribute("bandList",bandList);
+        return "band";
+    }
+}
