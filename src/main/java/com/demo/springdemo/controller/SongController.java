39d55a9ac993c275fa52da8b0a83e1a3ce4fdde1
Voicila Gabi <voicila40@gmail.com>
2022-07-11 12:48:59 +0300

    Tema 6 -adaugat functionalitate editare si stergere date din tabel song

--- src/main/java/com/demo/springdemo/controller/SongController.java
+++ src/main/java/com/demo/springdemo/controller/SongController.java
@@ -1,11 +1,15 @@
 package com.demo.springdemo.controller;
 
+import com.demo.springdemo.model.Band;
 import com.demo.springdemo.model.Song;
+import com.demo.springdemo.repository.BandRepository;
 import com.demo.springdemo.repository.SongRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -15,6 +19,9 @@ public class SongController {
     @Autowired
     SongRepository songRepository;
 
+    @Autowired
+    BandRepository bandRepository;
+
     //http://localhost:8080/song
     @GetMapping(value = "/song")
     public String song(Model model) {
@@ -22,6 +29,23 @@ public class SongController {
         model.addAttribute("songList", songList);
 
         return "song";
+    }
+
+    @PostMapping(value="/editSong")
+    public String editSong(@RequestParam("songId") int id, Model model) {
+        Song song = songRepository.findById(id).get();
+        model.addAttribute("song", song);
+
+        List<Band> bandList = bandRepository.findAll();
+        model.addAttribute("bandList", bandList);
+
+        return "addSong";     //songForm //va apela pagina html addSong
+    }
+
+    @PostMapping(value="/deleteSong")
+    public String deleteSong(@RequestParam("songId") int id) {
+        songRepository.deleteById(id);
+        return"redirect:/song";
     }
 
 }
